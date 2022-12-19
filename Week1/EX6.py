0069b0bceeabb5a4482704c78b11302985f0291d
Giant <isiri.sriuthai@gmail.com>
2022-12-20 01:23:09 +0700

    More testcase

--- Week1/EX6.py
+++ Week1/EX6.py
@@ -1,27 +1,53 @@
-panels = int(input())
-tables = []
-for i in range(panels):
-    tables.append([])
-    size = int(input())
-    for j in range(size):
-        temp = input()
-        tables[i].append(list(map(int,temp.split())))
+from unittest.mock import patch
+import unittest 
+class addTenTest(unittest.TestCase):
+    @patch('builtins.input', side_effect=['1', '3', '2 4 6', '2 4 6', '2 4 6'])
+    def test_1(self, input):
+        result = addTen()
+        assert result == [3]
 
-for table in tables:
-    s = len(table)
-    c = [[] for i in range(s)]
-    for row in table:
-        for e in range(s):
-            c[e].append(row[e])
-    table+=c
+    @patch('builtins.input', side_effect=['1', '4', '1 2 3 4', '1 2 3 4', '1 2 3 4', '1 2 3 4'])
+    def test_2(self, input):
+        result = addTen()
+        assert result == [4]
 
-for table in tables:
-    count = 0
-    for row in table:
-        for i in range(len(row)):
-            for j in range(2,len(row)+1):
-                if i+j <= len(row):
-                    if sum(row[i:i+j]) == 10:
-                        #print(row[i:i+j])
-                        count += 1
-    print(count)
+    @patch('builtins.input', side_effect=['2', '4', '1 2 3 4', '1 2 3 4', '1 2 3 4', '1 2 3 4'
+                                        , '5', '2 2 2 2 2', '2 2 2 2 2', '2 2 2 2 2', '2 2 2 2 2', '2 2 2 2 2'])
+    def test_3(self, input):
+        result = addTen()
+        assert result == [4,10]
+
+def addTen():
+    panels = int(input())
+    tables = []
+    countt = []
+    for i in range(panels):
+        tables.append([])
+        size = int(input())
+        for j in range(size):
+            temp = input()
+            tables[i].append(list(map(int,temp.split())))
+
+    for table in tables:
+        s = len(table)
+        c = [[] for i in range(s)]
+        for row in table:
+            for e in range(s):
+                c[e].append(row[e])
+        table+=c
+
+    for table in tables:
+        count = 0
+        for row in table:
+            for i in range(len(row)):
+                for j in range(2,len(row)+1):
+                    if i+j <= len(row):
+                        if sum(row[i:i+j]) == 10:
+                            #print(row[i:i+j])
+                            count += 1
+        #print(count)
+        countt.append(count)
+    return countt
+
+if __name__ == '__main__':
+    unittest.main()
--- Week1/EX7.py
+++ Week1/EX7.py
@@ -1,57 +1,88 @@
-file = open('ex7.txt','r')
-content = file.readlines()
-file.close()
-for i in range(len(content)):
-    content[i] = content[i].strip()
+from unittest.mock import mock_open,patch
+import unittest
 
-line1 = content[0].split()
-n = int(line1[0])
-c1 = int(line1[1])
-c2 = int(line1[2])
-#print(n,c1,c2)
+class solveTest(unittest.TestCase):
+    def test_1(self):
+        data = '''16 200 -10
+                12
+                70
+                1
+                999
+                50
+                20
+                1000
+                150
+                300
+                200
+                90
+                900
+                40
+                140
+                130
+                30'''
+        with patch('__main__.open', mock_open(read_data=data)) as m:
+            with open('foo') as h:
+                answer = solve(h)
+        assert answer == (50, 150, 20, 70, 90, 40, 130, 30)
 
-A = [int(i) for i in content[1:]]
-A.sort()
-#print(A)
+def solve(file):
+    #file = open('ex7.txt','r')
+    content = file.readlines()
+    for i in range(len(content)):
+        content[i] = content[i].strip()
 
-ansc1 = []
-for i in A:
-    for j in A:
-        if i+j == c1:
-            ansc1.append((i,j))
-#print(ansc1) #(x1,x2)
+    line1 = content[0].split()
+    n = int(line1[0])
+    c1 = int(line1[1])
+    c2 = int(line1[2])
+    #print(n,c1,c2)
 
-ansc2 = []
-for i in A:
-    for j in A:
-        if i-j == c2:
-            ansc2.append((i,j))
-#print(ansc2) #(x8,x6)
+    A = [int(i) for i in content[1:]]
+    A.sort()
+    #print(A)
 
-ansx1 = []
-for i in ansc1:
-    for j in A:
-        for k in A:
-            if i[0] == j-k:
-                ansx1.append((i[0],i[1],k,j))
-#print(ansx1) #(x1,x2,x3,x4)
+    ansc1 = []
+    for i in A:
+        for j in A:
+            if i+j == c1:
+                ansc1.append((i,j))
+    #print(ansc1) #(x1,x2)
 
-ansx6 = []
-for i in ansc2:
-    for j in A:
-        for k in A:
-            if i[1] == j-k:
-                ansx6.append((k,i[1],j,i[0]))
-#print(ansx6) #(x5,x6,x7,x8)
+    ansc2 = []
+    for i in A:
+        for j in A:
+            if i-j == c2:
+                ansc2.append((i,j))
+    #print(ansc2) #(x8,x6)
 
-pos_ans = []
-for i in ansx1:
-    temp = list(i)
-    for j in ansx6:
-        temp2 = temp+list(j)
-        stemp2 = set(temp2)
-        if len(temp2) == len(stemp2):
-            pos_ans.append(tuple(temp2))
+    ansx1 = []
+    for i in ansc1:
+        for j in A:
+            for k in A:
+                if i[0] == j-k:
+                    ansx1.append((i[0],i[1],k,j))
+    #print(ansx1) #(x1,x2,x3,x4)
 
-for i in pos_ans[0]:
-    print(i)
+    ansx6 = []
+    for i in ansc2:
+        for j in A:
+            for k in A:
+                if i[1] == j-k:
+                    ansx6.append((k,i[1],j,i[0]))
+    #print(ansx6) #(x5,x6,x7,x8)
+
+    pos_ans = []
+    for i in ansx1:
+        temp = list(i)
+        for j in ansx6:
+            temp2 = temp+list(j)
+            stemp2 = set(temp2)
+            if len(temp2) == len(stemp2):
+                pos_ans.append(tuple(temp2))
+
+    # for i in pos_ans[0]:
+    #     print(i)
+    return pos_ans[0]
+
+if __name__ == '__main__':
+    unittest.main()
