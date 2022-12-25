fec307cd3fde2a34bce6414bbbe0652d0d16b98d
Nathamon <110463038+Ploiiiz@users.noreply.github.com>
2022-12-26 01:29:44 +0700

    -

--- API/stocks.py
+++ API/stocks.py
@@ -1,10 +1,14 @@
+# Alpha Vantage
+
+
+
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
 
 ts = TimeSeries(key='7743c81996msh2ad1ff32ce0021ap1d042djsn21d3138a2fb0', output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='5min', outputsize='full')
 data['4. close'].plot()
-plt.title('Intraday Times Series for the MSFT stock (1 min)')
+plt.title('Intraday Times Series for the MSFT stock (5 min)')
 plt.show()
 
 # import http.client
@@ -38,3 +42,28 @@ plt.show()
 # data = res.read()
 
 # print(data.decode("utf-8"))
+
+# import requests
+
+# # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+# url = 'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=tesco&apikey=demo'
+# r = requests.get(url)
+# data = r.json()
+
+# print(data)
+
+
+
+# import csv
+# import requests
+
+# # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+# CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey=demo'
+
+# with requests.Session() as s:
+#     download = s.get(CSV_URL)
+#     decoded_content = download.content.decode('utf-8')
+#     cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+#     my_list = list(cr)
+#     for row in my_list:
+#         print(row)
