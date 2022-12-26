ebd16fdc7d7936dfd6764eaa32d71e79ae7eceec
Nathamon <110463038+Ploiiiz@users.noreply.github.com>
2022-12-26 11:46:52 +0700

    Create test.py

---
+++ API/test.py
@@ -0,0 +1,50 @@
+import requests
+
+# Set the API endpoint URL
+url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data"
+
+# Set the API key
+api_key = "7743c81996msh2ad1ff32ce0021ap1d042djsn21d3138a2fb0"
+
+# Set the query parameters
+params = {
+    "frequencyType": "daily",
+    "frequency": 1,
+    "startDate": "2022-01-01",
+    "endDate": "2022-01-31",
+    "symbol": "AAPL"
+}
+
+# Set the request headers
+headers = {
+    "X-RapidAPI-Key": api_key
+}
+
+# Make the API request
+response = requests.get(url, params=params, headers=headers)
+
+# Check the status code of the response
+if response.status_code == 200:
+    # If the request is successful, print the data
+    data = response.json()
+    print(data)
+else:
+    # If the request is unsuccessful, print the error message
+    print(response.text)
+
+
+# import http.client
+
+# conn = http.client.HTTPSConnection("yh-finance-complete.p.rapidapi.com")
+
+# headers = {
+#     'X-RapidAPI-Key': "7743c81996msh2ad1ff32ce0021ap1d042djsn21d3138a2fb0",
+#     'X-RapidAPI-Host': "yh-finance-complete.p.rapidapi.com"
+#     }
+
+# conn.request("GET", "/investp?conversion=eur-usd", headers=headers)
+
+# res = conn.getresponse()
+# data = res.read()
+
+# print(data.decode("utf-8"))
