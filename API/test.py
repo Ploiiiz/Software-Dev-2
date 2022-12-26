b244391220daa3121a6e00f010d04d67ed9d3f0d
Nathamon <110463038+Ploiiiz@users.noreply.github.com>
2022-12-26 12:01:45 +0700

    Update test.py

--- API/test.py
+++ API/test.py
@@ -1,49 +1,49 @@
-# import requests
-
-# # Set the API endpoint URL
-# url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data"
-
-# # Set the API key
-# api_key = "7743c81996msh2ad1ff32ce0021ap1d042djsn21d3138a2fb0"
-
-# # Set the query parameters
-# params = {
-#     "frequencyType": "daily",
-#     "frequency": 1,
-#     "startDate": "2022-01-01",
-#     "endDate": "2022-01-31",
-#     "symbol": "AAPL"
-# }
-
-# # Set the request headers
-# headers = {
-#     "X-RapidAPI-Key": api_key
-# }
-
-# # Make the API request
-# response = requests.get(url, params=params, headers=headers)
-
-# # Check the status code of the response
-# if response.status_code == 200:
-#     # If the request is successful, print the data
-#     data = response.json()
-#     print(data)
-# else:
-#     # If the request is unsuccessful, print the error message
-#     print(response.text)
-
-
 import requests
 
-url = "https://yh-finance-complete.p.rapidapi.com/investp"
+# Set the API endpoint URL
+url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data"
 
-querystring = {"conversion":"eur-usd"}
+# Set the API key
+api_key = "7743c81996msh2ad1ff32ce0021ap1d042djsn21d3138a2fb0"
 
-headers = {
-	"X-RapidAPI-Key": "7743c81996msh2ad1ff32ce0021ap1d042djsn21d3138a2fb0",
-	"X-RapidAPI-Host": "yh-finance-complete.p.rapidapi.com"
+# Set the query parameters
+params = {
+    "frequencyType": "daily",
+    "frequency": 1,
+    "startDate": "2022-01-01",
+    "endDate": "2022-01-31",
+    "symbol": "AAPL"
 }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+# Set the request headers
+headers = {
+    "X-RapidAPI-Key": api_key
+}
 
-print(response.text)
+# Make the API request
+response = requests.get(url, params=params, headers=headers)
+
+# Check the status code of the response
+if response.status_code == 200:
+    # If the request is successful, print the data
+    data = response.json()
+    print(data)
+else:
+    # If the request is unsuccessful, print the error message
+    print(response.text)
+
+
+# import requests
+
+# url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
+
+# querystring = {"q":"tesla","region":"US"}
+
+# headers = {
+# 	"X-RapidAPI-Key": "7743c81996msh2ad1ff32ce0021ap1d042djsn21d3138a2fb0",
+# 	"X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+# }
+
+# response = requests.request("GET", url, headers=headers, params=querystring)
+
+# print(response.text)
